16d07e6627e2d5575b9e953f15e4e1e24ade0703
Maja Kudlicka <maja.kudlicka@gmail.com>
2019-01-20 10:16:09 +0000

    feat- presentations notes and examples

---
+++ test.js
@@ -0,0 +1,31 @@
+// b();
+// console.log(a);
+//
+// var a = 'Hello World!';
+//
+// function b() {
+//     console.log('Called b!');
+// }
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+//
+// a=b=c;
+//
+// console.log({a, b, c});
+//
+//
+// console.log(1<2<3);
+// console.log(3<2<1);
+
+// function getPerson() {
+//
+//     return
+//     {
+//         firstname: 'Maja'
+//     }
+//
+// }
+//
+// console.log(getPerson());
